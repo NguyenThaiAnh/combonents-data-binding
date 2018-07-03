a0ca22695d11ababde76a53782a000d5edf58858
DESKTOP-FAJ9S7D\13120 <thaianhkhtn@gmail.com>
2018-07-03 22:39:35 +0700

    comment code

--- prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient }                                                     from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  /*
+  * khai báo 2 biến nameInput, amountInput kiểu ViewChild dùng để lấy giá trị
+  * */
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  // Khai báo biến này để bắn event ra bên component cha nhận và thêm vào mảng
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    /*
+    * Lấy giá trị của 2 biến ở ngoài view
+    * */
+    const ingname = this.nameInput.nativeElement.value;
+    const ingamount = this.amountInput.nativeElement.value;
+
+    const newIngredient = new Ingredient(ingname, ingamount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }

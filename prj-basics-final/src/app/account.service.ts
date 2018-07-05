04a96493d0718b60b3826e9af53264e5ca53b954
DESKTOP-FAJ9S7D\13120 <thaianhkhtn@gmail.com>
2018-07-05 22:26:23 +0700

    injecting service into service

--- prj-basics-final/src/app/account.service.ts
+++ prj-basics-final/src/app/account.service.ts
@@ -1,3 +1,7 @@
+import { LoggingService } from './logging.service';
+import { Injectable }     from '@angular/core';
+
+@Injectable()
 export class AccountService {
   accounts = [
     {name: 'Master account', status: 'active'},
@@ -5,11 +9,15 @@ export class AccountService {
     {name: 'Hidden account', status: 'hidden'},
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
--- prj-basics-final/src/app/account/account.component.ts
+++ prj-basics-final/src/app/account/account.component.ts
@@ -17,6 +17,5 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
